55a25b6c8c0d24114c8bf14eac3a75493a33db74
Trac Nguyen <nguyendtrac@gmail.com>
2018-09-07 21:21:36 +0000

    automatically backed up by learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,38 @@
+class SongsController < ApplicationController
+  def show 
+    @song = Song.find(params[:id])
+  end
+
+  def new 
+    @song = Song.new
+  end
+
+  def create 
+    @song = Song.new(song_params)
+
+    if @song.valid?
+      @song.save
+      redirect_to song_path(song_params)
+    else
+      render :new
+    end
+  end
+
+  def update 
+    @song = Song.find(params[:id])
+    @song.update(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else 
+      render :edit
+    end
+  end
+
+  private
+
+  def song_params
+    params.permit(:title, :released, :release_year, :artist_name, :genre)
+  end
+  
+end
