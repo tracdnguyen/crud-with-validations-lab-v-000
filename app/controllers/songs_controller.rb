44ee25d2411a710e1e07e67524a0be79c0f3c748
Trac Nguyen <nguyendtrac@gmail.com>
2018-09-07 21:32:37 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -37,7 +37,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.permit(:title, :released, :release_year, :artist_name, :genre)
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
   end
   
 end
